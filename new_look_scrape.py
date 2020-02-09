fddfeed06bbce3d11980eda0f7f89ee53a081900
Karen-Trippler <59242269+Karen-Trippler@users.noreply.github.com>
2020-02-09 10:06:56 +0000

    Add files via upload

--- new_look_scrape.py
+++ new_look_scrape.py
@@ -66,13 +66,13 @@ def get_co2 (textile):
 		df1.loc[i] = [textile, textile_data[textile]]
 		i=i+1
 		value = df1.get(df1["Textiles"] == textile)
-    return value.at[0,"CO2e"]
+	return value.at[0,"CO2e"]
 
 def calculation(text_dict):
     n = len(text_dict)
     overall_co2 = 0
     for all in text_dict:
-        co2_textile = get_co2(all)
-        textile_perc = text_dict[all]
-        overall_co2 += co2_textile * (textile_perc)/100
+    	co2_textile = get_co2(all.upper())
+    	textile_perc = text_dict[all]
+    	overall_co2 += co2_textile * (textile_perc)/100
     return overall_co2
